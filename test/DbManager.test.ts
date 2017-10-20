80feaf7e4b5103871589d5e1a89d8b58488a7dd5
Balázs Gallay <Balazs.Gallay@Provident.Hu>
2017-10-20 18:49:20 +0200

    Files added

---
+++ test/DbManager.test.ts
@@ -0,0 +1,74 @@
+import * as chai from 'chai';
+import { suite, test } from 'mocha-typescript';
+import { DbManager, TableInfo } from '../src';
+
+@suite
+export class DbManagerTest {
+
+  @test('Testing of the test framework...')
+  public t0() {
+    chai.expect(true).to.be.eq(true);
+  }
+
+  @test('Declare DBManager without initialization')
+  public t1() {
+    const dbManager: DbManager = null;
+    chai.expect(dbManager).to.be.eq(null, 'DbManager should be null');
+  }
+
+  @test('Initialize DBManager instance without parameters')
+  public t2() {
+    const dbManager = () => new DbManager(null, null);
+    chai.expect(dbManager).to.throw(DbManager.NULL_OR_EMPTY_NAME);
+    chai.expect(dbManager).not.to.throw(DbManager.NULL_OR_EMPTY_TABLES);
+  }
+
+  @test('Initialize DBManager with `empty string` db name and `null` tableInfo array')
+  public t3() {
+    const dbManager = () => new DbManager('', null);
+    chai.expect(dbManager).to.throw(DbManager.NULL_OR_EMPTY_NAME);
+    chai.expect(dbManager).not.to.throw(DbManager.NULL_OR_EMPTY_TABLES);
+  }
+
+  @test('Initialize DBManager with `undefined` db name and `null` tableInfo array')
+  public t4() {
+    const dbManager = () => new DbManager(undefined, null);
+    chai.expect(dbManager).to.throw(DbManager.NULL_OR_EMPTY_NAME);
+    chai.expect(dbManager).not.to.throw(DbManager.NULL_OR_EMPTY_TABLES);
+  }
+
+  @test('Initialize DBManager instance with valid name parameter and `null` tableInfo array')
+  public t5() {
+    const dbManager = () => new DbManager('db', null);
+    chai.expect(dbManager).not.to.throw(DbManager.NULL_OR_EMPTY_NAME);
+    chai.expect(dbManager).to.throw(DbManager.NULL_OR_EMPTY_TABLES);
+  }
+
+  @test('Initialize DBManager instance with valid name parameter and `undefined` tableInfo array')
+  public t6() {
+    const dbManager = () => new DbManager('db', undefined);
+    chai.expect(dbManager).not.to.throw(DbManager.NULL_OR_EMPTY_NAME);
+    chai.expect(dbManager).to.throw(DbManager.NULL_OR_EMPTY_TABLES);
+  }
+
+  @test('Initialize DBManager instance with valid name parameter and `empty array` tableInfo array')
+  public t7() {
+    const dbManager = () => new DbManager('db', []);
+    chai.expect(dbManager).not.to.throw(DbManager.NULL_OR_EMPTY_NAME);
+    chai.expect(dbManager).to.throw(DbManager.NULL_OR_EMPTY_TABLES);
+  }
+
+  @test('Initialize DBManager instance with only table parameter')
+  public t8() {
+    const dbManager = () => new DbManager(null, [new TableInfo('TableName', 'CustomerID', 'CustomerID')]);
+    chai.expect(dbManager).to.throw(DbManager.NULL_OR_EMPTY_NAME);
+    chai.expect(dbManager).not.to.throw(DbManager.NULL_OR_EMPTY_TABLES);
+  }
+
+  @test('Initialize DBManager instance with valid parameters')
+  public t9() {
+    const dbManager = () => new DbManager('db', [new TableInfo('TableName', 'CustomerID', 'CustomerID')]);
+    chai.expect(dbManager).not.to.throw();
+  }
+
+}
---
+++ test/index.ts
@@ -0,0 +1,2 @@
+import indexedDB from 'fake-indexeddb';
+export * from './DbManager.test';
