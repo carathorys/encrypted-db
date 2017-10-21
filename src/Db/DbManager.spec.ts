33eeb0065923356d0024bc556bf156a396e4c9e2
Balázs Gallay <Balazs.Gallay@Provident.Hu>
2017-10-21 21:31:42 +0200

    changes

--- src/Db/DbManager.spec.ts
+++ src/Db/DbManager.spec.ts
@@ -1,26 +1,31 @@
 import { DbManager } from './DbManager';
 import { DbTable } from './DbTable';
 
-describe('DbManager', () => {
-  it('Should not initialize without the necessary parameters initialize', () => {
-    expect(() => new DbManager('', null)).toThrow();
-    expect(() => new DbManager(null, null)).toThrow();
-    expect(() => new DbManager(undefined, null)).toThrow();
+describe('DbManager construction', () => {
+  it('should throw a specific error when database name parameter is `null`, `undefined`, or `empty string`', () => {
+    expect(() => new DbManager(null, null)).toThrowError(DbManager.NULL_OR_EMPTY_NAME);
+    expect(() => new DbManager(null, undefined)).toThrowError(DbManager.NULL_OR_EMPTY_NAME);
+    expect(() => new DbManager(null, [])).toThrowError(DbManager.NULL_OR_EMPTY_NAME);
+    expect(() => new DbManager(null, [new DbTable('TableName', 'ID', 'ID')])).toThrowError(DbManager.NULL_OR_EMPTY_NAME);
 
-    expect(() => new DbManager('', undefined)).toThrow();
-    expect(() => new DbManager(null, undefined)).toThrow();
-    expect(() => new DbManager(undefined, undefined)).toThrow();
+    expect(() => new DbManager(undefined, null)).toThrowError(DbManager.NULL_OR_EMPTY_NAME);
+    expect(() => new DbManager(undefined, undefined)).toThrowError(DbManager.NULL_OR_EMPTY_NAME);
+    expect(() => new DbManager(undefined, [])).toThrowError(DbManager.NULL_OR_EMPTY_NAME);
+    expect(() => new DbManager(undefined, [new DbTable('TableName', 'ID', 'ID')])).toThrowError(DbManager.NULL_OR_EMPTY_NAME);
 
-    expect(() => new DbManager('', [])).toThrow();
-    expect(() => new DbManager(null, [])).toThrow();
-    expect(() => new DbManager(undefined, [])).toThrow();
+    expect(() => new DbManager('', null)).toThrowError(DbManager.NULL_OR_EMPTY_NAME);
+    expect(() => new DbManager('', undefined)).toThrowError(DbManager.NULL_OR_EMPTY_NAME);
+    expect(() => new DbManager('', [])).toThrowError(DbManager.NULL_OR_EMPTY_NAME);
+    expect(() => new DbManager('', [new DbTable('TableName', 'ID', 'ID')])).toThrowError(DbManager.NULL_OR_EMPTY_NAME);
+  });
 
-    expect(() => new DbManager('', [new DbTable('TableName', 'ID', 'ID')])).toThrow();
-    expect(() => new DbManager(null, [new DbTable('TableName', 'ID', 'ID')])).toThrow();
-    expect(() => new DbManager(undefined, [new DbTable('TableName', 'ID', 'ID')])).toThrow();
+  it('should throw a specific error when table list is null, or empty, but the database name is OK', () => {
+    expect(() => new DbManager('db', null)).toThrowError(DbManager.NULL_OR_EMPTY_TABLES);
+    expect(() => new DbManager('db', undefined)).toThrowError(DbManager.NULL_OR_EMPTY_TABLES);
+    expect(() => new DbManager('db', [])).toThrowError(DbManager.NULL_OR_EMPTY_TABLES);
   });
 
   it('Should successfully initialize with these parameters', () => {
-    expect(() => new DbManager('Database', [new DbTable('TableName', 'ID', 'ID')])).not.toThrow();
+    expect(() => new DbManager('db', [new DbTable('TableName', 'ID', 'ID')])).not.toThrow();
   });
 });
